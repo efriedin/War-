b93ed005bbb43c12a628ffef23fb681482837cfd
Erin Friedman <erin.friedman@ymail.com>
2023-03-06 12:36:47 -0600

    code outline

---
+++ war.js
@@ -0,0 +1,24 @@
+//create class for Deck
+class Deck {
+    constructor(suits,values){
+        const suits = ['S','D','C','H']
+    }
+}
+    //constructors suit and value
+//create class for Player
+    //constructors player1 and player2
+//create class for Card
+
+//turns array 1-26
+
+//create function too shuffle cards
+
+
+//create function to deal cards
+    //26 to each player
+    
+//funtion to iterate through turns array
+    //player plays 1 card each turn
+    //player with the higher card is awarded 1 point
+    //do nothing for a tie
+    //display score and declare the winner
