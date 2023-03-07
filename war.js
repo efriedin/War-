53e838fe7a46c5962491714d68b2cedeae28be80
Erin Friedman <erin.friedman@ymail.com>
2023-03-06 19:41:51 -0600

    corrected creating player hand to not repeat cards

--- war.js
+++ war.js
@@ -2,7 +2,7 @@
 class Deck {
     constructor(){
         this.suits = ['Spade','Diamond','Clubs','Hearts'];
-        this.values = ["A", "2", "3", "4", "5", "6", "7", "8", "9", "10", "J", "Q", "K"];      
+        this.values = ["A", "2", "3", "4", "5", "6", "7", "8", "9", "10", "J", "Q", "K"];
     }
     //Create deck 
     createDeck(){
@@ -11,7 +11,7 @@ class Deck {
             for(let x = 0; x < this.values.length; x++){
                 let card = "Value: " + this.values[x] + ", " + "Suit: " + this.suits[i];
                 deck.push(card);
-            } console.log("HERE : "+this.cardIndex);
+            }
         } return deck;
     }   
 }
@@ -37,8 +37,12 @@ class Player{
     getHand(){
         return this.playerHand;
     }
+
+    getCard(cardIndex){
+        return this.playerHand[cardIndex];
+    }
     //method for removing card from hand
-    //method for assigning points 
+    //method for assigning points
     addPoints(){
         this.playerPoints++;
     }
@@ -46,102 +50,85 @@ class Player{
     //method for getting points
     getPoints(){
         return this.playerPoints;
-    }
+    } 
+    
 }
 
 //create class for Card
 class Card {
     constructor (deck){
         this.deck = deck;
-        this.cardIndex = [...Array(52).keys()];  
     }
     pickCard(){
         //random pick card index from given deck array
-        console.log("Length"+deck.length);
-        let randomIndex = Math.floor(Math.random() * this.cardIndex.length);
-        let randomCard = this.cardIndex[randomIndex];
-        //remove card
-        this.cardIndex.pop(randomIndex);
+        const randomCard = Math.floor(Math.random() * this.deck.length);
         //return card
         return (randomCard);
     }
-    getCard(cardIndex){
-        return this.deck[cardIndex];
     }
-
-    //update function
-    updateCardIndex(){
-        this.cardIndex.pop(index);
-    }
-}
     
 
 //main
 let deckObj = new Deck ();
 let deck = deckObj.createDeck();
 console.log(deck);
-let cardObj = new Card(deck);
+let cardObj = new Card (deck);
+
 
 //console.log(cardIndex);
 //console.log(deck[cardIndex]);
 let player1 = new Player(0, []);
 let player2 = new Player(0, []);
 
-//create player hands
-let tempDeck = deck.slice();
-console.log(typeof(tempDeck));
-let cardObjTemp = new Card (tempDeck);
-for (let i = 0; i < 25; i++){
-    //add player1 cards
-    let cardIndex = cardObjTemp.pickCard();
-    player1.addCard(cardIndex);
-    console.log(cardIndex);     
-    //tempDeck.pop(tempDeck[cardIndex]);
-    
-    //add player2 cards
-    cardIndex = cardObjTemp.pickCard();
-    player2.addCard(cardIndex);
-    console.log(cardIndex);
-    //tempDeck.pop(tempDeck[cardIndex]);
-}
 
-    
-  
+//create player hands
+for (let i = 0; i < 26; i++){
+    //add player1 cards
+    let cardIndex = cardObj.pickCard();
+    player1.addCard(deck[cardIndex]);
+    deck.splice(cardIndex, 1);
+    console.log("length: " + deck.length);
+    //add player2 cards
+    cardIndex = cardObj.pickCard();
+    player2.addCard(deck[cardIndex]);
+    deck.splice(cardIndex, 1);
+    console.log("length: " + deck.length);
+}
 player1.viewhand();
 player2.viewhand();
-console.log(tempDeck);
+console.log(deck);
 
 //initializing card class to randomly select from each players hand 
 let player1Hand = new Card (player1.getHand());
 let player2Hand = new Card (player2.getHand());
 //pick random card from player1's hand
-let player1Card = player1Hand.pickCard();
-console.log(cardObj.getCard(player1Card));
+let player1CardIndex = player1Hand.pickCard();
+console.log(player1.getCard(player1CardIndex));
 //pick random card from player 2's hand
-let player2Card = player2Hand.pickCard();
-console.log(cardObj.getCard(player2Card));
+let player2CardIndex = player2Hand.pickCard();
+console.log(player2.getCard(player2CardIndex));
 
 //adjust values of each player's card
 let compareP1 = 0;
 let compareP2 = 0;
-if(player1Card > 12 && player1Card < 26 ){
-    compareP1 = player1Card - 13;
-} else if (player1Card > 25 && player1Card < 39){
-    compareP1 = player1Card - 26;
-}else if(player1Card > 45 && player1Card < 52){
-    compareP1 = player1Card - 39;
+if(player1CardIndex > 12 && player1CardIndex < 26 ){
+    compareP1 = player1CardIndex - 13;
+} else if (player1CardIndex > 25 && player1CardIndex < 39){
+    compareP1 = player1CardIndex - 26;
+}else if(player1CardIndex > 45 && player1CardIndex < 52){
+    compareP1 = player1CardIndex - 39;
 }else {
-    compareP1 = player1Card;
+    compareP1 = player1CardIndex;
 }
 
-if(player2Card > 12 && player2Card < 26 ){
-    compareP2 = player2Card - 13;
-} else if (player2Card > 25 && player2Card < 39){
-    compareP2 = player2Card - 26;
-}else if(player2Card > 45 && player2Card < 52){
-    compareP2 = player2Card - 39;
+if(player2CardIndex > 12 && player2CardIndex < 26 ){
+    compareP2 = player2CardIndex - 13;
+} else if (player2CardIndex > 25 && player2CardIndex < 39){
+    compareP2 = player2CardIndex - 26;
+}else if(player2CardIndex > 45 && player2CardIndex < 52){
+    compareP2 = player2CardIndex - 39;
 }else {
-    compareP2 = player2Card;
+    compareP2 = player2CardIndex;
 }
 //compare and assign points to player with higher valued card
 if(compareP1 > compareP2){
@@ -153,6 +140,7 @@ if(compareP1 > compareP2){
 } else {
     console.log("It's a tie!");
 }
+
 //remove card from player1's hand 
 //remove card from player2's hand
 
